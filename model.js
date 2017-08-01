84d7b1e3a72ba51c71ad56d80dc15a09b6e335dd
Melody Plan <melodyplan@gmail.com>
2017-07-31 21:25:10 -0700

    made outfit schema

--- model.js
+++ model.js
@@ -2,4 +2,35 @@ const mongoose = require('mongoose')
 
 const outfitSchema = mongoose.Schema({
   // not sure what this would be-- if it would list photos?
-})
+  //wait! just kidding. image = link = string right?? =D
+  headpiece: {type: String, required: false},
+  body: {type: String, required: false},
+  bottom: {type: String, required: false},
+  shoes: {type: String, required: false},
+  accessories: {
+    accessoryOne: String,
+    accessoryTwo: String,
+    accessoryThree: String,
+    accessoryFour: String,
+    accessoryFive: String,
+    required: false
+    //not sure what to do if they have more than five accessories?
+  },
+  publishDate: {type: Date, default: Date.now()}
+});
+
+outfitSchema.methods.apiRepr = function() {
+  return {
+    id: this._id,
+    headpiece: this.headpiece,
+    body: this.body,
+    bottom: this.bottom,
+    shoes: this.shoes,
+    accessories: this.accessories,
+    publishDate: this.publishDate
+  };
+}
+
+const Outfit = mongoose.mode('Outfit', outfitSchema);
+
+modult.exports = {Outfit};
