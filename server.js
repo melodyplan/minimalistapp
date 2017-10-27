1555af6e0e2aa3c296a2b8e01c44b20379e533e0
Melody Plan <melodyplan@gmail.com>
2017-10-27 11:15:15 -0700

    added index

--- server.js
+++ server.js
@@ -1,67 +1,67 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 
-mongoose.Promise = global.Promise;
+mongoose.Promise = global.Promise
 
 const { PORT, DATABASE_URL } = require('./config')
 const { Outfit } = require('./model')
 
-const app = express();
-app.use(bodyParser.json());
+const app = express()
+app.use(express.static('public'))
+app.use(bodyParser.json())
 
 app.get('/outfits', (req, res) => {
   console.log('outfits')
-  Outfit
-    .find()
+  Outfit.find()
     .then(outfits => {
       console.log('outfits found')
       res.json(outfits)
     })
     .catch(err => {
-      console.error(err);
-      res.status(400).json({message: 'Bad request error: a field is missing'});
+      console.error(err)
+      res.status(400).json({ message: 'Bad request error: a field is missing' })
     })
 })
 
-// app.use(express.static('public'));
-let server;
+let server
 
 function runServer(databaseUrl = DATABASE_URL, port = PORT) {
   return new Promise((resolve, reject) => {
     mongoose.connect(databaseUrl, err => {
       if (err) {
-        return reject(err);
+        return reject(err)
       }
-      server = app.listen(port, () => {
-        console.log(`Your app is listening on ${port}`);
-        resolve();
-      })
-      .on('error', err => {
-        mongoose.disconnect();
-        reject(err);
-      });
-    });
-  });
+      server = app
+        .listen(port, () => {
+          console.log(`Your app is listening on ${port}`)
+          resolve()
+        })
+        .on('error', err => {
+          mongoose.disconnect()
+          reject(err)
+        })
+    })
+  })
 }
 
 //close server and return promise
 function closeServer() {
   return mongoose.disconnect().then(() => {
     return new Promise((resolve, reject) => {
-      console.log('Closing server');
+      console.log('Closing server')
       server.close(err => {
         if (err) {
-          return reject(err);
+          return reject(err)
         }
-        resolve();
-      });
-    });
-  });
+        resolve()
+      })
+    })
+  })
 }
 
 if (require.main === module) {
-  runServer().catch(err => console.error(err));
-};
+  runServer().catch(err => console.error(err))
+}
 
-module.exports = {app, runServer, closeServer};
+module.exports = { app, runServer, closeServer }
