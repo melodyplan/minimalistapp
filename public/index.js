f2cf854947ed96de705052d35fb38e84c1980fc0
Melody Plan <melodyplan@gmail.com>
2017-10-29 11:40:19 -0700

    added form and ajax

--- public/index.js
+++ public/index.js
@@ -1,3 +1,28 @@
 $('.submit').click(function(event) {
-  event.preventDefault().val()
+  event.preventDefault()
+  let headpiece = $('.headpiece-input').val()
+  let body = $('.body-input').val()
+  let bottom = $('.bottom-input').val()
+  let shoes = $('.shoes-input').val()
+  let accessories = $('.accessories-input').val()
+  console.log(headpiece, body, bottom, shoes, accessories)
 })
+
+function addOutfit(headpiece, body, bottom, shoes, accessories, callback) {
+  $.ajax({
+    url: '/outfits',
+    contentType: 'application/json',
+    type: 'POST',
+    dataType: 'json',
+    data: JSON.stringify({
+      headpiece: headpiece,
+      body: body,
+      bottom: bottom,
+      shoes: shoes,
+      accessories: accessories
+    }),
+    success: function(data) {
+      callback()
+    }
+  })
+}
