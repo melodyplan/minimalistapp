19d74e8eb5111cf482f8be925821c1915559d5d0
Melody Plan <melodyplan@gmail.com>
2017-10-29 11:58:13 -0700

    added addOutfit function

--- public/index.js
+++ public/index.js
@@ -1,28 +1,30 @@
-$('.submit').click(function(event) {
-  event.preventDefault()
-  let headpiece = $('.headpiece-input').val()
-  let body = $('.body-input').val()
-  let bottom = $('.bottom-input').val()
-  let shoes = $('.shoes-input').val()
-  let accessories = $('.accessories-input').val()
-  console.log(headpiece, body, bottom, shoes, accessories)
-})
-
-function addOutfit(headpiece, body, bottom, shoes, accessories, callback) {
-  $.ajax({
-    url: '/outfits',
-    contentType: 'application/json',
-    type: 'POST',
-    dataType: 'json',
-    data: JSON.stringify({
+function addOutfit() {
+  $('.submit').click(function(event) {
+    event.preventDefault()
+    let headpiece = $('.headpiece-input').val()
+    let body = $('.body-input').val()
+    let bottom = $('.bottom-input').val()
+    let shoes = $('.shoes-input').val()
+    let accessories = $('.accessories-input').val()
+    const outfitData = {
       headpiece: headpiece,
       body: body,
       bottom: bottom,
       shoes: shoes,
       accessories: accessories
-    }),
+    };
+    console.log(headpiece, body, bottom, shoes, accessories)
+  });
+
+  $.ajax({
+    url: '/outfits',
+    contentType: 'application/json',
+    type: 'POST',
+    dataType: 'json',
+    data: JSON.stringify(outfitData),
     success: function(data) {
-      callback()
-    }
-  })
-}
+      console.log("post request worked!")
+    };
+  });
+
+};
