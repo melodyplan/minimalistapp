e85206f2ab16d6b93566cb5babfccaacb1872229
Melody Plan <melodyplan@gmail.com>
2017-11-01 17:55:05 -0700

    changed db name

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
-exports.DATABASE_URL = process.env.DATABASE_URL ||
-                        global.DATABASE_URL ||
-                        'mongodb://localhost/minimalist-app'
-exports.PORT = process.env.PORT || 8080;
+exports.DATABASE_URL =
+  process.env.DATABASE_URL ||
+  global.DATABASE_URL ||
+  'mongodb://localhost/minimalistapp'
+exports.PORT = process.env.PORT || 8080
